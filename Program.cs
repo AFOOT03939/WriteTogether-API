c12cf935b6a1ed7c8dbe845c6067efb188c69416
LCRMTZ <luccraft13@gmail.com>
2026-04-28 14:16:45 -0600

    #1 configure CORS policy to allow requests from frontend origins

--- Program.cs
+++ Program.cs
@@ -26,6 +26,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173", "https://writetogether-app.onrender.com")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // 🔐 Authentication (MOVER AQUÍ)
 builder.Services.AddAuthentication(options =>
 {
@@ -114,6 +124,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseCors("AllowFrontend");
 
 app.UseAuthentication();
 app.UseAuthorization();
